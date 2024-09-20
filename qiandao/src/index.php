43e8ca6115d2dbe50b5c9db3794ea2925c0d582f
aikunaaa <143726710+aikunaaa@users.noreply.github.com>
2024-09-20 21:23:59 +0800

    Add files via upload

---
+++ qiandao/src/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="zh">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>∫Ÿ∫Ÿ∫Ÿ~~</title>
+    <script>
+    document.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+    });
+
+    "FLAGFLAGFLAG";
+
+
+    document.addEventListener('keydown', function(e) {
+        // F12 key
+        if (e.keyCode === 123) {
+            e.preventDefault();
+        }
+
+        if ((e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) || (e.ctrlKey && e.keyCode ===
+                85)) {
+            e.preventDefault();
+        }
+    });
+
+
+    document.addEventListener('keydown', function(e) {
+        if (e.ctrlKey && e.shiftKey && e.keyCode === 67) {
+            e.preventDefault();
+        }
+    });
+    </script>
+</head>
+
+<body>
+    <h1>ﬂ’ﬂ„ﬂœ</h1>
+    <p>ﬂ’ﬂ„ﬂ’ﬂ„~</p>
+</body>
+
+
+</html>
